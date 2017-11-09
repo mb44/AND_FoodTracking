26ef5892cceb36cd89043977ca1581964acab588
Morten Beuchert <mortenbgood@gmail.com>
2017-11-09 20:39:30 +0100

    Add FoodTemplates to array list

--- app/src/main/java/com/example/mb/and_foodtracking/Tab2Fragment.java
+++ app/src/main/java/com/example/mb/and_foodtracking/Tab2Fragment.java
@@ -6,28 +6,50 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.GridView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class Tab2Fragment extends Fragment {
     private static final String TAG = "Tab2Fragment";
 
     private Button btnTEST;
 
+    private ArrayList<FoodTemplate>foodTemplates;
+    private FoodTemplateAdapter arrayAdapter;
+    private GridView gridView;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.tab2_fragment,container,false);
 
-        /*
-        btnTEST = (Button) view.findViewById(R.id.btnTEST2);
+        FoodTemplate food1 = new FoodTemplate("Cow", "2 week", R.drawable.cow);
+        FoodTemplate food2 = new FoodTemplate("Pig", "3 week", R.drawable.pig);
+        FoodTemplate food3 = new FoodTemplate("Vegetables", "1 week", R.drawable.vegetables);
 
-        btnTEST.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getActivity(), "TESTING BUTTON CLICK 1",Toast.LENGTH_SHORT).show();
-            }
-        });
+        FoodTemplate food4 = new FoodTemplate("Milk", "1 week", R.drawable.milk);
+        FoodTemplate food5 = new FoodTemplate("Cashew Nuts", "2 week", R.drawable.cashew);
+        FoodTemplate food6 = new FoodTemplate("Sugar", "60 week", R.drawable.sugar);
+
+        foodTemplates = new ArrayList<>();
+        foodTemplates.add(food1);
+        foodTemplates.add(food2);
+        foodTemplates.add(food3);
+        foodTemplates.add(food4);
+        foodTemplates.add(food5);
+        foodTemplates.add(food6);
+
+        /*
+        MainActivity context = (MainActivity)getActivity();
+        arrayAdapter = new FoodTemplateAdapter(context, foodTemplates);
+
+        gridView = (GridView)context.findViewById(R.id.food_template_view);
+        gridView.setNumColumns(2);
+        gridView.setAdapter(arrayAdapter);
         */
 
         return view;
