1d3a086efbe636adfe69d207df89510e718d3a00
Morten Beuchert <mortenbgood@gmail.com>
2017-11-09 20:38:14 +0100

    Minor changes to code

--- app/src/main/java/com/example/mb/and_foodtracking/MainActivity.java
+++ app/src/main/java/com/example/mb/and_foodtracking/MainActivity.java
@@ -18,15 +18,21 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
 import android.widget.TableLayout;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import static com.example.mb.and_foodtracking.R.id.food_template_view;
 
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
 
     private SectionsPageAdapter mSectionsPageAdapter;
-    private ViewPager  mViewPager;
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreate: Starting.");
 
+        //mSectionsPageAdapter = new SectionsPageAdapter(this, getSupportFragmentManager());
         mSectionsPageAdapter = new SectionsPageAdapter(getSupportFragmentManager());
 
         // set up the ViewPager with the sections adapter
@@ -42,16 +49,14 @@ public class MainActivity extends AppCompatActivity {
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(mViewPager);
-
     }
 
     private void  setupViewPager(ViewPager viewPager)
     {
-        SectionsPageAdapter adapter = new SectionsPageAdapter(getSupportFragmentManager());
-
-        adapter.addFragment(new Tab1Fragment(), "Menu");
-        adapter.addFragment(new Tab2Fragment(), "Register food");
-        adapter.addFragment(new Tab3Fragment(), "Unregister food");
-        viewPager.setAdapter(adapter);
+        //SectionsPageAdapter adapter = new SectionsPageAdapter(this, getSupportFragmentManager());
+        mSectionsPageAdapter.addFragment(new Tab1Fragment(), "Menu");
+        mSectionsPageAdapter.addFragment(new Tab2Fragment(), "Register food");
+        mSectionsPageAdapter.addFragment(new Tab3Fragment(), "Unregister food");
+        viewPager.setAdapter(mSectionsPageAdapter);
     }
 }
