81d6d11162503156acd14e1beaa2096f976e4aee
Lars La Cour <lars@robotit.dk>
2017-11-14 13:23:31 +0100

    NFC read and white reimplemented, as small code blocks.

--- app/src/main/java/com/example/mb/and_foodtracking/MainActivity.java
+++ app/src/main/java/com/example/mb/and_foodtracking/MainActivity.java
@@ -1,54 +1,81 @@
 package com.example.mb.and_foodtracking;
 
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.FormatException;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.MifareUltralight;
+import android.nfc.tech.Ndef;
+import android.os.Parcelable;
 import android.support.design.widget.TabLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v7.app.AppCompatActivity;
+
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-
-import android.widget.ArrayAdapter;
-import android.widget.GridView;
-import android.widget.TableLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import java.util.ArrayList;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.UUID;
 
-import static com.example.mb.and_foodtracking.R.id.food_template_view;
+import static com.example.mb.and_foodtracking.R.id.textTab1;
+
+
+//import static com.example.mb.and_foodtracking.R.id.food_template_view;
 
 
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "MainActivity";
 
+    private static final String TAG = "MainActivity";
     private SectionsPageAdapter mSectionsPageAdapter;
     private ViewPager mViewPager;
+    NfcAdapter nfcAdapter;
+    PendingIntent pendingIntent;
+    IntentFilter[] intentFiltersArray;
+    String[][] techListArray;
+    Boolean isWriting = false;
+    Boolean isClearing = false;
+
+    Intent intent;
+    TextView mainTextView;
+    Tag detectedTag;
+    private static final String EMPTY_TAG_STRING = "This is an empty food tag";
+    private String uniqueID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreate: Starting.");
+        setContentView(R.layout.activity_main);
+        initUserInterface();
+        pendingIntent = PendingIntent.getActivity(this, 0, new Intent(this,getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP),0);
 
-        //mSectionsPageAdapter = new SectionsPageAdapter(this, getSupportFragmentManager());
-        mSectionsPageAdapter = new SectionsPageAdapter(getSupportFragmentManager());
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        IntentFilter ndef = new IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED);
 
-        // set up the ViewPager with the sections adapter
-        mViewPager = (ViewPager) findViewById(R.id.container);
-        setupViewPager(mViewPager);
+        detectedTag = getIntent().getParcelableExtra((NfcAdapter.EXTRA_TAG));
 
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
-        tabLayout.setupWithViewPager(mViewPager);
+
+        try {
+            ndef.addDataType("*/*");
+            //    ndef2.addDataType("MIME_TEXT_PLAIN");
+        }catch (IntentFilter.MalformedMimeTypeException e){
+            throw new RuntimeException("fail", e);
+        }
+        intentFiltersArray = new IntentFilter[]{ndef, /*ndef2*/ };
+        techListArray = new String[][] {new String[]{MifareUltralight.class.getName()}};
+
+        detectedTag = getIntent().getParcelableExtra((NfcAdapter.EXTRA_TAG));
     }
 
     private void  setupViewPager(ViewPager viewPager)
@@ -58,5 +85,190 @@ public class MainActivity extends AppCompatActivity {
         mSectionsPageAdapter.addFragment(new Tab2Fragment(), "Register food");
         mSectionsPageAdapter.addFragment(new Tab3Fragment(), "Unregister food");
         viewPager.setAdapter(mSectionsPageAdapter);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG,"state: onStart");
+        //readNFC(getIntent());                    // read the content on the NFC tag
+        //readNFC(intent);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG,"state: onPause");
+        nfcAdapter.disableForegroundDispatch(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG,"state: onResume");
+        nfcAdapter.enableForegroundDispatch(this, pendingIntent, intentFiltersArray, techListArray);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        Log.d(TAG,"state: onNewIntent");
+        initUserInterface();
+        Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+        Toast.makeText(this,"On new intent", Toast.LENGTH_LONG).show();
+
+        try {
+            writeToNFC("det er en abe", tag);
+            Toast.makeText(MainActivity.this,"write successful", Toast.LENGTH_LONG).show();
+        }catch (Exception e){
+            e.printStackTrace();
+            Toast.makeText(MainActivity.this,"Fail to write", Toast.LENGTH_LONG).show();
+        }
+
+/*
+        Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+
+        if(isWriting && intent.getParcelableExtra(NfcAdapter.EXTRA_TAG) != null)
+        {
+            setNewDate(tag);
+        }
+        else if(isClearing && intent.getParcelableExtra(NfcAdapter.EXTRA_TAG) != null)
+        {
+            eraseTag(tag);
+        }
+
+        readNFC(intent);
+*/
+    }
+
+    private void setNewDate(Tag tag)
+    {
+        try {
+            writeToNFC("The current date and time is:\n " +currentDateString()+ "\n unique ID: "+generateUniqueId()+"\n", tag);  // whrite or overwrite the content on the NFC tag
+            Toast.makeText(MainActivity.this,"write successful", Toast.LENGTH_LONG).show();
+        }catch (Exception e){
+            e.printStackTrace();
+            Toast.makeText(MainActivity.this,"Fail to write", Toast.LENGTH_LONG).show();
+        }
+        isWriting = false;
+
+    }
+
+    private void eraseTag(Tag tag)
+    {
+        try {
+            writeToNFC(EMPTY_TAG_STRING, tag);  // whrite or overwrite the content on the NFC tag
+            Toast.makeText(MainActivity.this,"erase successful", Toast.LENGTH_LONG).show();
+        }catch (Exception e){
+            e.printStackTrace();
+            Toast.makeText(MainActivity.this,"Fail to erase", Toast.LENGTH_LONG).show();
+        }
+        isClearing = false;
+    }
+
+
+    public void readNFC(Intent intent) {
+
+        if (NfcAdapter.ACTION_TECH_DISCOVERED.equals(intent.getAction())) {
+            mainTextView.setText("Empty NFCtag is discovered");
+        }
+
+        else if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(intent.getAction())) {
+
+            Parcelable[] rawMessages = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+
+            for (Parcelable rawMessage : rawMessages) {
+                NdefMessage message = (NdefMessage) rawMessage;
+                NdefRecord[] records = message.getRecords();
+
+                for (NdefRecord record : records) {
+                    if (record.getTnf() == NdefRecord.TNF_WELL_KNOWN && Arrays.equals(record.getType(), NdefRecord.RTD_TEXT)) {
+                        byte[] payload = record.getPayload();
+                        String textEncoding = ((payload[0] & 128) == 0) ? "UTF-8" : "UTF-16";
+                        int languageCodeLength = payload[0] & 0063;
+                        try {
+                            String tagText = new String(payload, languageCodeLength + 1, payload.length - languageCodeLength - 1, textEncoding);
+                            //mainTextView.setText(tagText); // toDo: show the text
+                            Toast.makeText(this, tagText, Toast.LENGTH_LONG).show();
+                        } catch (UnsupportedEncodingException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+    private void setupNFC()
+    {
+
+    }
+
+    private void initUserInterface()
+    {
+        mainTextView = (TextView) findViewById(R.id.textTab1);
+        mSectionsPageAdapter = new SectionsPageAdapter(getSupportFragmentManager());
+
+        //mSectionsPageAdapter = new SectionsPageAdapter(this, getSupportFragmentManager());
+
+
+        // set up the ViewPager with the sections adapter
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        setupViewPager(mViewPager);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
+    }
+
+    public void writeToNFC(String text, Tag tag) throws IOException, FormatException {
+        NdefRecord[] records = {createRecord(text)};
+        NdefMessage message = new NdefMessage(records);
+        Ndef ndef = Ndef.get(tag);
+        ndef.connect();
+        ndef.writeNdefMessage(message);
+        ndef.close();
+    }
+
+    private NdefRecord createRecord(String text)throws UnsupportedEncodingException {
+        String lang = "en";
+        byte[] textBytes = text.getBytes();
+        byte[] langBytes = lang.getBytes("US-ASCII");
+        int langLength = langBytes.length;
+        int textLength = textBytes.length;
+        byte[] payload = new byte[1 + langLength + textLength];
+        payload[0] = (byte) langLength;
+
+        System.arraycopy(langBytes,0,payload,1,langLength);
+        System.arraycopy(textBytes,0,payload,1 + langLength, textLength);
+
+        return new NdefRecord(NdefRecord.TNF_WELL_KNOWN,NdefRecord.RTD_TEXT,new byte[0],payload);
+    }
+
+    private String currentDateString(){
+        String currentDate = Calendar.getInstance().getTime().toString();
+        return currentDate;
+    }
+
+    private String generateUniqueId()
+    {
+        return uniqueID = UUID.randomUUID().toString();// unique id generator
+    }
+
+    public void clearTag (View v)
+    {
+        isClearing = true;
+        isWriting = false;
+        Toast.makeText(MainActivity.this,"The clear tag is clicked",Toast.LENGTH_LONG).show();
+
+        Log.d(TAG,"This is the clear tag");
+    }
+
+    public void addDate (View v)
+    {
+        isWriting = true;
+        isClearing = false;
+        Toast.makeText(MainActivity.this,"The add date tag is clicked",Toast.LENGTH_LONG).show();
+        Log.d(TAG, currentDateString());
     }
 }
